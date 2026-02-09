65b8a5bcdc9197e4587609312bee3dc0f7883ffb
kitoha <kitoha7@gmail.com>
2026-02-09 15:32:48 +0900

    Update Testcontainers and image versions for integration tests

--- common/src/testFixtures/kotlin/com/eventticketserver/common/IntegrationTestSupport.kt
+++ common/src/testFixtures/kotlin/com/eventticketserver/common/IntegrationTestSupport.kt
@@ -3,7 +3,7 @@ package com.eventticketserver.common
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
-import org.testcontainers.containers.KafkaContainer
+import org.testcontainers.kafka.ConfluentKafkaContainer
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
 import com.redis.testcontainers.RedisContainer
@@ -12,9 +12,9 @@ import com.redis.testcontainers.RedisContainer
 class IntegrationTestSupport {
 
     companion object {
-        private const val POSTGRES_IMAGE = "postgres:15-alpine"
-        private const val REDIS_IMAGE = "redis:7-alpine"
-        private const val KAFKA_IMAGE = "confluentinc/cp-kafka:7.5.0"
+        private const val POSTGRES_IMAGE = "postgres:15.10-alpine3.21"
+        private const val REDIS_IMAGE = "redis:7.4.2-alpine3.21"
+        private const val KAFKA_IMAGE = "confluentinc/cp-kafka:7.5.3"
     }
 
     @Bean
@@ -31,7 +31,7 @@ class IntegrationTestSupport {
 
     @Bean
     @ServiceConnection
-    fun kafkaContainer(): KafkaContainer {
-        return KafkaContainer(DockerImageName.parse(KAFKA_IMAGE))
+    fun kafkaContainer(): ConfluentKafkaContainer {
+        return ConfluentKafkaContainer(DockerImageName.parse(KAFKA_IMAGE))
     }
 }
